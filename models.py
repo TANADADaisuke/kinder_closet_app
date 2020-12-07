92aecd05d1885575ace52ad3410aec7bdd139ecd
Tanada Daisuke <nvkx873110@gmail.com>
2020-12-08 07:25:45 +0900

    datatype definition

--- models.py
+++ models.py
@@ -1,63 +1,70 @@
 import os
-from sqlalchemy import Column, String, create_engine, Integer
+from sqlalchemy import Column, String, create_engine, Integer, Float, Datetime
 from flask_sqlalchemy import SQLAlchemy
 import json
+from datetime import datetime
 
 database_path = os.environ['DATABASE_URL']
 
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
 def setup_db(app, database_path=database_path):
+    """binds a flask application and a SQLAlchemy service
+
+    Returns: db
+    """
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    # db.create_all()
+
     return db
 
 
-'''
-Person
-Have title and release year
-'''
-class Person(db.Model):  
-  __tablename__ = 'people'
+# --------------------------------------------- #
+# Clothes
+# Have type, size, and registered time
+# --------------------------------------------- #
+class Clothes(db.Model):  
+    __tablename__ = 'clothes'
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
+    id = Column(Integer, primary_key=True)
+    type = Column(String(120), nullable=False)
+    size = Column(Float, nullable=False)
+    registered_time = Column(Datetime, nullable=False)
 
-  def __init__(self, name, catchphrase=""):
-    self.name = name
-    self.catchphrase = catchphrase
+    def __init__(self, type, size):
+        self.type = type
+        self.size = size
+        self.registered_time = datetime.utcnow()
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'catchphrase': self.catchphrase}
+    def format(self):
+        return {
+            'id': self.id,
+            'type': self.type,
+            'size': self.size,
+            'registerd': registered_time
+        }
 
-'''
-User
-Have title and release year
-'''
+
+# --------------------------------------------- #
+# User
+# Have e-mail and address
+# --------------------------------------------- #
 class User(db.Model):  
-  __tablename__ = 'users'
+    __tablename__ = 'users'
 
-  id = Column(Integer, primary_key=True)
-  e_mail = Column(String)
-  address = Column(String)
+    id = Column(Integer, primary_key=True)
+    e_mail = Column(String(120), nullable=False)
+    address = Column(String(500))
 
-  def __init__(self, e_mail, address=""):
-    self.e_mail = e_mail
-    self.address = address
+    def __init__(self, e_mail, address=""):
+        self.e_mail = e_mail
+        self.address = address
 
-  def format(self):
-    return {
-      'id': self.id,
-      'e_mail': self.e_mail,
-      'address': self.address}
+    def format(self):
+        return {
+            'id': self.id,
+            'e_mail': self.e_mail,
+            'address': self.address
+        }
